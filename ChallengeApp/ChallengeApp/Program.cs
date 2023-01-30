a2bc1ae0d6299c6330c01f81a2872b4335fee6bb
AndrzejLakomski <alakomski@op.pl>
2023-01-31 00:59:17 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+bool isWoman = false;
+var age = 16;
+
+
+if (isWoman && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+
+else if (isWoman && name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (!isWoman && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+  
+

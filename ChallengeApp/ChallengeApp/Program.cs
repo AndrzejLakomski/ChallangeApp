2b819f2e3e460189ec624f5ce3f87bf11c89c2c3
AndrzejLakomski <alakomski@op.pl>
2023-01-31 01:07:21 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -2,12 +2,10 @@
 bool isWoman = false;
 var age = 16;
 
-
 if (isWoman && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-
 else if (isWoman && name == "Ewa" && age == 33)
 {
     Console.WriteLine("Ewa lat 33");
@@ -16,5 +14,3 @@ else if (!isWoman && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
 }
-  
-

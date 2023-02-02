5498d59bbb3d7cfc73a3181e9b29dc2d4e2cc018
AndrzejLakomski <alakomski@op.pl>
2023-02-02 23:22:30 +0100

    Zadanie dzień 6 - Klasa - else if

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,30 @@
+﻿namespace ChallengeApp
+{
+    public class User
+    {
+        private List<int> points = new List<int>();
+
+        public User(string name, string surname, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Age = age;
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+        
+        public int Result
+        {
+            get
+            {
+                return this.points.Sum();
+            }
+
+        }
+        public void AddPoint(int point)
+        {
+            this.points.Add(point);
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,29 +1,34 @@
-﻿
-int number = 112333600;
-string numberAsString = number.ToString();
-char[] signs = numberAsString.ToArray();
+﻿using ChallengeApp;
 
-List<char> numbers = new List<char>();
+User user1 = new User("Alojzy", "Pietruszka", 18);
+User user2 = new User("Baltazar", "Gąbka", 46);
+User user3 = new User("Mieszko", "Pierwszy", 148);
 
-numbers.Add('0');
-numbers.Add('1');
-numbers.Add('2');
-numbers.Add('3');
-numbers.Add('4');
-numbers.Add('5');
-numbers.Add('6');
-numbers.Add('7');
-numbers.Add('8');
-numbers.Add('9');
+user1.AddPoint(1);
+user1.AddPoint(2);
+user1.AddPoint(5);
+user2.AddPoint(6);
+user2.AddPoint(3);
+user2.AddPoint(1);
+user3.AddPoint(21);
+user3.AddPoint(1);
+user3.AddPoint(4);
 
-Console.WriteLine("Sprawdzana liczba to " + number);
+var result = user1.Result;
 
-foreach (var sign in numbers)
+var result2 = user2.Result;
+
+var result3 = user3.Result;
+
+if (result > result2)
 {
-    var count = 0;
-    foreach (var number2 in signs)
+    Console.WriteLine("Najwyższy zdobyty wynik to " + result + ". Należy do pracownika " + user1.Name + " " + user1.Surname + ", lat " + user1.Age);
+}
+else if (result2 > result3 )
     {
-        if (sign == number2) count++;
+    Console.WriteLine("Najwyższy zdobyty wynik to " + result2 + ". Należy do pracownika " + user2.Name + " " + user2.Surname + ", lat " + user2.Age);
     }
-    Console.WriteLine("Cyfra " + sign + " => " + count);
+else
+{ 
+    Console.WriteLine("Najwyższy zdobyty wynik to " + result3 + ". Należy do pracownika " + user3.Name + " " + user3.Surname + ", lat " + user3.Age); 
 }
